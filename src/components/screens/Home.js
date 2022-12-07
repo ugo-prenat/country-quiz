4134ee5a5329eb6f29895ec30c8bee88dceae152
Infernaton <lubin.pauly@gmail.com>
2022-12-07 16:13:00 +0100

    Test Icon home

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Pressable } from 'react-native';
-import { styles } from '../../styles/styles';
+import React from "react";
+import { Icon } from "@rneui/themed";
+import { SafeAreaView, View, Text, Pressable } from "react-native";
+import { styles } from "../../styles/styles";
 
 const Home = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View>
         <Text>Home</Text>
-        <Pressable onPress={() => navigation.push('register')}>
+        <Pressable onPress={() => navigation.push("register")}>
           <Text>go to register</Text>
         </Pressable>
+        <Icon name="account-box" color="#000" />
       </View>
     </SafeAreaView>
   );
