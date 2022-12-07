6b4b8df985e8e67a66a3cbea97b247b521993459
Brian <58400498+nairb725@users.noreply.github.com>
2022-12-07 17:01:05 +0100

    api get all

--- src/components/screens/List.js
+++ src/components/screens/List.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { styles } from '../../styles/styles';
 
-const List = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <Text>List</Text>
-    </SafeAreaView>
-  );
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://restcountries.com/v3.1/name/france');
+      const json = await response.json();
+      setData(json);
+      console.log(json)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+//   return (
+//     <View style={{ flex: 1, padding: 24 }}>
+//       {isLoading ? <ActivityIndicator/> : (
+//         <FlatList
+//           data={data}
+//           keyExtractor={({ id }, index) => id}
+//           renderItem={({ item }) => (
+//             <Text>{item.title}, {item.releaseYear}</Text>
+//           )}
+//         />
+//       )}
+//     </View>
+//   );
 };
-export default List;
